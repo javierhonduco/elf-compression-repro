0f33df7ae4506ab3a9926f5b7a48e48f559d6364
Francisco Javier Honduvilla Coto <javierhonduco@gmail.com>
2022-07-12 12:36:13 +0100

    temp fix

--- main.go
+++ main.go
@@ -12,17 +12,23 @@ func main() {
 	if err != nil {
 		panic("could not open elf")
 	}
+	raw, _ := os.Open(os.Args[1])
 	defer elfFile.Close()
 	for _, s := range elfFile.Sections {
 		if s.Flags&elf.SHF_COMPRESSED == 0 {
 			continue
 		}
 
-		reader := s.Open()
+		//reader := s.Open()
+		raw.Seek(int64(s.Offset), os.SEEK_SET)
+
 		var compression uint32
-		if err := binary.Read(reader, binary.LittleEndian, &compression); err != nil {
+		if err := binary.Read(raw, binary.LittleEndian, &compression); err != nil {
 			continue
 		}
+
+		//reader.Seek(int64(s.Offset)+24, os.SEEK_SET)
+
 		fmt.Println("== section name:", s.Name, "compression:", compression)
 
 		if elf.CompressionType(compression) != elf.COMPRESS_ZLIB {
